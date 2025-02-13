76f439270ab1c8b7f097c268154eb36b141735a9
Shivang Mishra <githubshiv@gmail.com>
2025-02-13 16:30:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002354552656))
 
-OWNER_ID = int(getenv("OWNER_ID", 7045191057))
+OWNER_ID = int(getenv("OWNER_ID", 8056154987))
 
 OWNER = int(getenv("OWNER", 2105971379))
 
@@ -27,7 +27,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY","HK543fklqxgt66hvxf")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AbhiModszYT/AnieXEricaMusic",
+    "https://github.com/Shivmis/Super2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
